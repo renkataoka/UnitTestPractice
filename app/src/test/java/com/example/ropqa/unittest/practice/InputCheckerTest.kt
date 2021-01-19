d04b6981f73c79112db84d61ceba55a9b753c8c0
renkataoka <530lotus@gmail.com>
2021-01-19 22:45:08 +0900

    Modify InputCheckerTest.kt using AssertJ

--- app/src/test/java/com/example/ropqa/unittest/practice/InputCheckerTest.kt
+++ app/src/test/java/com/example/ropqa/unittest/practice/InputCheckerTest.kt
@@ -1,11 +1,10 @@
 package com.example.ropqa.unittest.practice
 
-import org.hamcrest.CoreMatchers.`is`
+import org.assertj.core.api.Assertions.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
-import org.junit.Assert.*
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import java.lang.IllegalArgumentException
@@ -24,43 +23,40 @@ class InputCheckerTest {
     }
 
     @Test
-    fun isValid() {
-        val actual = target.isValid("foo")
-        assertThat(actual, `is`(true))
-    }
-
-    @Test
     fun isValid_givenLessThan3_returnsFalse() {
         val actual = target.isValid("ab")
-        assertThat(actual, `is`(false))
+        assertThat(actual).isFalse()
     }
 
     @Test
     fun isValid_givenAlphabetic_returnsTrue() {
         val actual = target.isValid("abc")
-        assertThat(actual, `is`(true))
+        assertThat(actual).isTrue()
     }
 
     @Test
     fun isValid_givenNumeric_returnsTrue() {
         val actual = target.isValid("123")
-        assertThat(actual, `is`(true))
+        assertThat(actual).isTrue()
     }
 
     @Test
     fun isValid_givenInvalidCharacter_returnsFalse() {
         val actual = target.isValid("abc@123")
-        assertThat(actual, `is`(false))
+        assertThat(actual).isFalse()
     }
 
     @Test
     fun isValid_givenAlphaNumeric_returnsTrue() {
         val actual = target.isValid("abc123")
-        assertThat(actual, `is`(true))
+        assertThat(actual).isTrue()
     }
 
-    @Test(expected = IllegalArgumentException::class)
+    @Test
     fun isValid_givenNull_throwsIllegalArgumentException() {
-        target.isValid(null)
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { target.isValid(null) }
+            .withMessage("Cannot be null!")
+            .withNoCause()
     }
 }
